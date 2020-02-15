3a580b68fcc7ca7ad9a081fa69eee399eaf77eab
Tomas <sobotka.visualstudio@email.cz>
2020-02-15 12:30:07 +0100

    pár změn s pojmenováním

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 function zmenitFormular (){
     let typFormulare = document.getElementById("typFormulare").innerText;
-    if(typFormulare == "Zaregistrovat se"){
-        document.getElementById("prihlasovaciOblast").innerHTML = "<form method='POST' action=''><input id='jmeno' type='text' placeholder='Jméno...'><br><input type='password' placeholder='Heslo...'><br><input type='newPassword' placeholder='Nové heslo...'><br><input id='tlacitko' type='submit' value='Přihlásit se'></form><span onclick='zmenitFormular()' id='typFormulare'>Přihlásit se</span>";
-
+    if(typFormulare == "Nemáš účet ?"){
+        document.getElementById("prihlasovaciOblast").innerHTML = "<form method='POST' action=''><input id='jmeno' type='text' placeholder='Jméno...'><br><input type='password' placeholder='Heslo...'><br><input type='newPassword' placeholder='Nové heslo...'><br><input id='tlacitko' type='submit' value='Registrovat se'><br><span onclick='zmenitFormular()' id='typFormulare'>Již mám účet</span></form>";
+        document.getElementById("nadpisFormulare").innerHTML = "Registrace";
     }
     else{
-        document.getElementById("prihlasovaciOblast").innerHTML = "<form method='POST' action=''><input id='jmeno' type='text' placeholder='Jméno...'><br><input type='password' placeholder='Heslo...'><br><input id='tlacitko' type='submit' value='Přihlásit se'></form><span onclick='zmenitFormular()' id='typFormulare'>Zaregistrovat se</span>";
+        document.getElementById("prihlasovaciOblast").innerHTML = "<form method='POST' action=''><input id='jmeno' type='text' placeholder='Jméno...'><br><input type='password' placeholder='Heslo...'><br><input id='tlacitko' type='submit' value='Přihlásit se'><br><span onclick='zmenitFormular()' id='typFormulare'>Nemáš účet ?</span></form>";
+        document.getElementById("nadpisFormulare").innerHTML = "Přihlášení";
     }
 }
 
