c09778108f08451f73b7376fced06097b81cef46
Tomas <sobotka.visualstudio@email.cz>
2020-02-15 12:32:28 +0100

    oprava inputu

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function zmenitFormular (){
     let typFormulare = document.getElementById("typFormulare").innerText;
     if(typFormulare == "Nemáš účet ?"){
-        document.getElementById("prihlasovaciOblast").innerHTML = "<form method='POST' action=''><input id='jmeno' type='text' placeholder='Jméno...'><br><input type='password' placeholder='Heslo...'><br><input type='newPassword' placeholder='Nové heslo...'><br><input id='tlacitko' type='submit' value='Registrovat se'><br><span onclick='zmenitFormular()' id='typFormulare'>Již mám účet</span></form>";
+        document.getElementById("prihlasovaciOblast").innerHTML = "<form method='POST' action=''><input id='jmeno' type='text' placeholder='Jméno...'><br><input type='password' placeholder='Heslo...'><br><input type='Password' placeholder='Nové heslo...'><br><input id='tlacitko' type='submit' value='Registrovat se'><br><span onclick='zmenitFormular()' id='typFormulare'>Již mám účet</span></form>";
         document.getElementById("nadpisFormulare").innerHTML = "Registrace";
     }
     else{
